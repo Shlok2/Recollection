00086eb98ca2308e914a4ef9c38e74a573fba474
Shlok <81300534+Shlok2@users.noreply.github.com>
2023-01-09 09:35:37 +0530

    Delete client directory

--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import posts from './posts';
-
-// in here we will mention all the reducers we have.
-export default combineReducers({
-    posts: posts,
-});
